e669d07c199e0c1684769272b1c1b00bbc024d02
TheFatRatre <1981590178@qq.com>
2024-04-10 20:56:09 +0800

    翻页代码部分开发

--- src/main/java/com/cyc/imagon/Application.java
+++ src/main/java/com/cyc/imagon/Application.java
@@ -14,17 +14,17 @@ import javax.swing.*;
  * @Version 1.0
  */
 public class Application {
-    static MainModule mainModule = new MainModule();
-    public static ImageIcon image=new ImageIcon("src/main/resources/image/init.png");
+    public static MainModule mainModule = new MainModule();
+    public static ImageIcon initImage=new ImageIcon("src/main/resources/image/init.png");
+    public static JFrame frame = new JFrame("Imagon");
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Imagon");
         GUI gui = new GUI(mainModule);
         frame.setContentPane(gui.root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,800);
         frame.setVisible(true);
         //初始图像
-        JLabel label=new JLabel(image);
+        JLabel label=new JLabel(initImage);
         frame.setLayout(null);
         frame.add(label);
         label.setBounds(50,10,900,720);
